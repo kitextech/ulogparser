d2255619acbb961886d8190d0cb0c3e669e2cb68
Andreas Okholm <aokholm@gmail.com>
2017-07-28 22:25:59 +0200

    Initial Commit

---
+++ ulogReader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ulogReader
+//
+//  Created by Andreas Okholm on 28/07/2017.
+//  Copyright © 2017 Andreas Okholm. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
